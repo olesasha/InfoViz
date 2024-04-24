f8521b6bb33cf2bdb5d34440a6ef5141446e5404
Maximilian Hess <hess.max@gmx.de>
2024-04-24 08:19:18 +0200

    tested the pca

--- E2/app.py
+++ E2/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
 import json
+import pandas as pd
+from sklearn import decomposition,preprocessing
 
 app = Flask(__name__)
 
 # ensure that we can reload when we change the HTML / JS for debugging
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+df_teams_agg = pd.read_csv("E2/static/data/agg_teams.csv")
+df_player_scoring = pd.read_csv("E2/static/data/agg_player_scoring_stats.csv")
+df_data_post_3_point_line = pd.read_csv("E2/static/data/data_post_3_point_line.csv")
+
+scaler = preprocessing.StandardScaler()
+scaler.fit(df_teams_agg)
+
 
 
 
---
+++ E2/static/js/vis.js
@@ -0,0 +1,7 @@
+d3.select("#svg").append("circle")
+.attr("cx", 50)
+.attr("cy", 50)
+.attr("r", 40)
+.attr("stroke", "green")
+.attr("stroke-width", 4)
+.attr("fill", "yellow");
