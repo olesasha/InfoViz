ae30debfdae98fa58da8f8e9f7eff18eb3295bb6
olesasha <kravsasha01@gmail.com>
2024-06-01 16:32:28 +0200

    v7 globe

--- E3/static/js/globe.js
+++ E3/static/js/globe.js
@@ -8,9 +8,9 @@ function render_globe(globe_data) {
 
     var width = 500
     var height = 500
-    var sensitivity = 75
+    var sensitivity = 40    // dragging sens
     const config = {
-        speed: 0.005,
+        speed: 0.003,      // rotation speed
         verticalTilt: -30,
         horizontalTilt: 0};
     
@@ -36,53 +36,61 @@ function render_globe(globe_data) {
                     .attr("cy", height/2)
                     .attr("r", initialScale)
     
-                    svg.call(d3.drag().on('drag', () => {
-                        const rotate = projection.rotate()
-                        const k = sensitivity / projection.scale()
-                        projection.rotate([
-                          rotate[0] + d3.event.dx * k,
-                          rotate[1] - d3.event.dy * k
-                        ])
-                        path = d3.geoPath().projection(projection)
-                        svg.selectAll("path").attr("d", path)
-                      }))
-                        .call(d3.zoom().on('zoom', () => {
-                          if(d3.event.transform.k > 0.3) {
-                            projection.scale(initialScale * d3.event.transform.k)
-                            path = d3.geoPath().projection(projection)
-                            svg.selectAll("path").attr("d", path)
-                            globe.attr("r", projection.scale())
-                          }
-                          else {
-                            d3.event.transform.k = 0.3
-                          }
-                        }))
+    // dragging 
+    svg.call(d3.drag()
+    .on('drag', function(event) {
+        const rotate = projection.rotate();
+        // calculate the new rotation based on the drag distance
+        const k = sensitivity / projection.scale();
+        const newRotation = [
+            rotate[0] + event.dx * k, // update the longitude
+            rotate[1] - event.dy * k  // update the latitude
+        ];
+        projection.rotate(newRotation);
+
+
+        path = d3.geoPath().projection(projection);
+        svg.selectAll("path").attr("d", path); // redraw all paths with the new projection
+        }))
+        .call(d3.zoom()
+        .scaleExtent([0.3, Infinity])
+        .on('zoom', function(event) {
+            // update the scale of the projection based on zoom level
+            if (event.transform.k > 0.3) {
+                projection.scale(initialScale * event.transform.k);
+                path = d3.geoPath().projection(projection);
+                svg.selectAll("path").attr("d", path); // redraw all paths with the new scale
+                globe.attr("r", projection.scale()); // update the globe's radius
+            } else {
+                event.transform.k = 0.3; // prevent zooming out too much
+            }
+        }))
                     
-                      let map = svg.append("g")
+        let map = svg.append("g")
+    
+        let data = globe_data
+    
+        map.append("g")
+        .attr("class", "countries" )
+        .selectAll("path")
+        .data(data.features)
+        .enter().append("path")
+        .attr("class", d => "country_" + d.properties.name.replace(" ","_"))
+        .attr("d", path)
+        .attr("fill", "white")
+        .style('stroke', 'black')
+        .style('stroke-width', 0.3)
+        .style("opacity",0.8)
                     
-                      let data = globe_data
-                    
-                      map.append("g")
-                        .attr("class", "countries" )
-                        .selectAll("path")
-                        .data(data.features)
-                        .enter().append("path")
-                        .attr("class", d => "country_" + d.properties.name.replace(" ","_"))
-                        .attr("d", path)
-                        .attr("fill", "white")
-                        .style('stroke', 'black')
-                        .style('stroke-width', 0.3)
-                        .style("opacity",0.8)
-                    
-                      //rotate
-                      /*d3.timer(function(elapsed) {
-                        const rotate = projection.rotate()
-                        const k = sensitivity / projection.scale()
-                        projection.rotate([
-                          rotate[0] - 1 * k,
-                          rotate[1]
-                        ])
-                        path = d3.geoPath().projection(projection)
-                        svg.selectAll("path").attr("d", path)
-                      },200)*/
+        //rotate
+        d3.timer(function(elapsed) {
+        const rotate = projection.rotate()
+        const k = sensitivity / projection.scale()
+        projection.rotate([
+            rotate[0] - 1 * k,
+            rotate[1]
+        ])
+        path = d3.geoPath().projection(projection)
+        svg.selectAll("path").attr("d", path)
+        },200)
 }
